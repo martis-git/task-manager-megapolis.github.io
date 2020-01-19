9c5be3635e00fc6e40c219dafc8d07951c175d77
Ilya Azin <martis.azin@gmail.com>
2020-01-19 11:44:17 +0300

    Updates

---
+++ precache-manifest.6ff1d9da1f514f21da23ca340e768189.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0137cc1d3ff7b71ce41715b7ee956150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75e15ffd1077acac4345",
+    "url": "/static/css/4.4ddad681.chunk.css"
+  },
+  {
+    "revision": "79f357c0f2ad9746de8d",
+    "url": "/static/css/5.2b239dbf.chunk.css"
+  },
+  {
+    "revision": "9155bcafc1b05626e8ff",
+    "url": "/static/css/6.8d861f8b.chunk.css"
+  },
+  {
+    "revision": "63fb7e9483b4bc491ee7",
+    "url": "/static/css/main.5fc08fcb.chunk.css"
+  },
+  {
+    "revision": "1f863e080ab4834e86dc",
+    "url": "/static/js/0.32bbdc0e.chunk.js"
+  },
+  {
+    "revision": "1cfab7874f44c7c41683",
+    "url": "/static/js/3.c6dab46f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/3.c6dab46f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75e15ffd1077acac4345",
+    "url": "/static/js/4.dd058276.chunk.js"
+  },
+  {
+    "revision": "79f357c0f2ad9746de8d",
+    "url": "/static/js/5.9f622e5b.chunk.js"
+  },
+  {
+    "revision": "9155bcafc1b05626e8ff",
+    "url": "/static/js/6.3d9a1fef.chunk.js"
+  },
+  {
+    "revision": "63fb7e9483b4bc491ee7",
+    "url": "/static/js/main.6383f296.chunk.js"
+  },
+  {
+    "revision": "3f4c4aa9aa9eb6f2840c",
+    "url": "/static/js/runtime-main.6bcb51a3.js"
+  }
+]);
